5b6ae41e2ce818ab653bb2ad0304bc2892a09775
Marc Sánchez <sanchezfauste@gmail.com>
2017-09-19 18:00:31 +0200

    Creating django project and portal app

---
+++ portal/views.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+#######################################################################
+# bPortal is a SuiteCRM portal written using django project.
+
+# Copyright (C) 2017 Marc Sanchez Fauste
+# Copyright (C) 2017 BTACTIC, SCCL
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################
+
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from suitepy.suitecrm import SuiteCRM
+
+# Create your views here.
+
+def index(request):
+    accounts = SuiteCRM().get_bean_list('Accounts', max_results = 10)
+    template = loader.get_template('portal/index.html')
+    context = {
+        'accounts' : accounts
+    }
+    return HttpResponse(template.render(context, request))
