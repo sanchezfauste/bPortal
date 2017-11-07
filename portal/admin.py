6e4637f31925ac7fe907acbd30e5be252783fb4f
Marc Sánchez <sanchezfauste@gmail.com>
2017-11-07 19:59:50 +0100

    Adding Role and Permissions models

--- portal/admin.py
+++ portal/admin.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Layout
+from .models import Layout, Role, RolePermission, RoleUser
 
 # Register your models here.
 
 admin.site.register(Layout)
+admin.site.register(Role)
+admin.site.register(RolePermission)
+admin.site.register(RoleUser)
--- portal/models.py
+++ portal/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,3 +22,38 @@ class Layout(models.Model):
 
     def __str__(self):
         return self.module + ' - ' + self.view + ' view'
+
+class Role(models.Model):
+
+    name = models.CharField(max_length=50, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+class RolePermission(models.Model):
+
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
+    module = models.CharField(max_length=50)
+    ACTION_CHOICES = (
+        ('read', 'Read'),
+        ('create', 'Create'),
+        ('edit', 'Edit'),
+        ('delete', 'Delete'),
+    )
+    action = models.CharField(max_length=30, choices=ACTION_CHOICES)
+    grant = models.BooleanField()
+    order = models.IntegerField()
+
+    class Meta:
+        unique_together = ("role", "module", "action")
+
+    def __str__(self):
+        return self.role.name + ' - ' + self.action.title() + ' ' + self.module
+
+class RoleUser(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.role.name + ' - ' + self.user.username
