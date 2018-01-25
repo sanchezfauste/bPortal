90a69648f9782dda55e594db0c4386f55eda2d76
Marc Sánchez <sanchezfauste@gmail.com>
2018-01-25 19:22:54 +0100

    Improve edit detail layout screen

--- portal/static/portal/save_layouts.js
+++ portal/static/portal/save_layouts.js
@@ -51,3 +51,38 @@ function save_detail_layout() {
     });
 
 }
+
+function add_detail_row() {
+    $('#selected_fields_list_group').append('\
+        <div class="row">\
+            <div class="col card sortable-ul selected-field"></div>\
+            <div class="col card sortable-ul selected-field"></div>\
+            <div class="col col-2 text-center align-self-center">\
+                <a class="btn btn-danger" href="#" role="button" onclick="remove_detail_row($(this));">\
+                    <span class="oi oi-x"></span>\
+                </a>\
+            </div>\
+        </div>\
+    ');
+    $( ".selected-field, #available-fields" ).sortable({
+      connectWith: ".sortable-ul"
+    }).disableSelection();
+    $(".selected-field").each(function() {
+      $(this).on("sortreceive", function(event, ui) {
+        if ($(this).children().length > 1) {
+          $(ui.sender).sortable('cancel');
+        }
+      })
+    });
+}
+
+function remove_detail_row(object) {
+    var row = object.parent().parent();
+    row.children('div.col.card.sortable-ul').map(function() {
+        var children = $(this).children();
+        if (children.length > 0) {
+            $('#available-fields').append(children);
+        }
+    })
+    row.remove()
+}
