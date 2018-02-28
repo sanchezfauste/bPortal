6315548f30910e2f350aba8794ea94b66864b08b
Marc Sánchez <sanchezfauste@gmail.com>
2018-02-28 22:15:44 +0100

    Updating to bootstrap 4.0.0

--- bPortal/settings.py
+++ bPortal/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portal.apps.PortalConfig',
-    'bootstrap4',
+    'portal.apps.PortalConfig'
 ]
 
 MIDDLEWARE = [
