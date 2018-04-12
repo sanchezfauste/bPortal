eccdc4352cf7a7adf3b21b4a3a2c3f3769a5ce67
Marc Sánchez <sanchezfauste@gmail.com>
2018-04-12 17:52:15 +0200

    Adding useful scripts to deal with i18n

---
+++ make_messages.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#######################################################################
+# bPortal is a SuiteCRM portal written using django project.
+
+# Copyright (C) 2017-2018 BTACTIC, SCCL
+# Copyright (C) 2017-2018 Marc Sanchez Fauste
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################
+
+echo -e -n "#############################################\n"
+echo -e -n "#        bPortal - A SuiteCRM portal        #\n"
+echo -e -n "#                                           #\n"
+echo -e -n "#   Copyright (C) 2017-2018 BTACTIC, SCCL   #\n"
+echo -e -n "#   Copyright (C) 2017-2018 Marc Sanchez    #\n"
+echo -e -n "#############################################\n\n"
+
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+
+echo -e -n "Processing portal app locale messages...\n"
+cd $ABSDIR/portal
+django-admin makemessages -a
+
+echo -e -n "\nProcessing bPortal project locale messages...\n"
+cd $ABSDIR/bPortal
+django-admin makemessages -a
